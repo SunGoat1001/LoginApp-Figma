b1a66877db8b51bbce4e54b85f14d03c3e4e68aa
SunGoat1001 <ngoctien10012004@gmail.com>
2024-03-04 09:58:06 +0700

    Initial commit

---
+++ app/src/main/java/exportkit/figma/slide_2_activity.java
@@ -0,0 +1,147 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		slide_2
+	 *	@date 		Saturday 02nd of March 2024 03:01:07 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.loginapp.R;
+
+	public class slide_2_activity extends Activity {
+
+	
+	private View _bg__slide_2;
+	private View _bg____bar_ek1;
+	private View _bg__right_side_ek1;
+	private View _bg__battery_ek1;
+	private ImageView rectangle;
+	private ImageView combined_shape;
+	private ImageView rectangle_ek1;
+	private ImageView wifi;
+	private ImageView mobile_signal;
+	private View _bg__left_side_ek1;
+	private View _bg____time_ek1;
+	private TextView _9_41;
+	private TextView log_in;
+	private TextView sign_up;
+	private ImageView line_1;
+	private TextView your_email;
+	private TextView password;
+	private TextView don_t_have_an_account__sign_up;
+	private TextView forgot_password_;
+	private TextView wrong_password;
+	private TextView contact_dscodetech_com;
+	private View rectangle_13;
+	private View rectangle_16;
+	private View rectangle_17;
+	private View rectangle_14;
+	private View _bg__fi_rr_eye_crossed_ek1;
+	private ImageView vector;
+	private View rectangle_15;
+	private TextView continuee;
+	private TextView or;
+	private ImageView line_2;
+	private ImageView line_3;
+	private View _bg__group_1_ek1;
+	private TextView login_with_apple;
+	private ImageView pngwing_1;
+	private View _bg__group_2_ek1;
+	private TextView login_with_google;
+	private ImageView pngwing_2;
+	private View _bg__group_3_ek1;
+	private View ellipse_1;
+	private View ellipse_2;
+	private View ellipse_3;
+	private View ellipse_4;
+	private View ellipse_5;
+	private View ellipse_6;
+	private View ellipse_7;
+	private View ellipse_8;
+	private View ellipse_9;
+	private View ellipse_10;
+	private View ellipse_11;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.slide_2);
+
+		
+		_bg__slide_2 = (View) findViewById(R.id._bg__slide_2);
+//		_bg____bar_ek1 = (View) findViewById(R.id._bg____bar_ek1);
+//		_bg__right_side_ek1 = (View) findViewById(R.id._bg__right_side_ek1);
+//		_bg__battery_ek1 = (View) findViewById(R.id._bg__battery_ek1);
+//		rectangle = (ImageView) findViewById(R.id.rectangle);
+//		combined_shape = (ImageView) findViewById(R.id.combined_shape);
+//		rectangle_ek1 = (ImageView) findViewById(R.id.rectangle_ek1);
+//		mobile_signal = (ImageView) findViewById(R.id.mobile_signal);
+//		_bg__left_side_ek1 = (View) findViewById(R.id._bg__left_side_ek1);
+//		_bg____time_ek1 = (View) findViewById(R.id._bg____time_ek1);
+//		_9_41 = (TextView) findViewById(R.id._9_41);
+		log_in = (TextView) findViewById(R.id.log_in);
+		sign_up = (TextView) findViewById(R.id.sign_up);
+		line_1 = (ImageView) findViewById(R.id.line_1);
+		your_email = (TextView) findViewById(R.id.your_email);
+		password = (TextView) findViewById(R.id.password);
+//		don_t_have_an_account__sign_up = (TextView) findViewById(R.id.don_t_have_an_account__sign_up);
+		forgot_password_ = (TextView) findViewById(R.id.forgot_password_);
+		wrong_password = (TextView) findViewById(R.id.wrong_password);
+		contact_dscodetech_com = (TextView) findViewById(R.id.contact_dscodetech_com);
+		rectangle_13 = (View) findViewById(R.id.rectangle_13);
+		rectangle_16 = (View) findViewById(R.id.rectangle_16);
+		rectangle_17 = (View) findViewById(R.id.rectangle_17);
+		rectangle_14 = (View) findViewById(R.id.rectangle_14);
+		_bg__fi_rr_eye_crossed_ek1 = (View) findViewById(R.id._bg__fi_rr_eye_crossed_ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		rectangle_15 = (View) findViewById(R.id.rectangle_15);
+		continuee = (TextView) findViewById(R.id.continuee);
+		or = (TextView) findViewById(R.id.or);
+		line_2 = (ImageView) findViewById(R.id.line_2);
+		line_3 = (ImageView) findViewById(R.id.line_3);
+//		_bg__group_1_ek1 = (View) findViewById(R.id._bg__group_1_ek1);
+		login_with_apple = (TextView) findViewById(R.id.login_with_apple);
+		pngwing_1 = (ImageView) findViewById(R.id.pngwing_1);
+//		_bg__group_2_ek1 = (View) findViewById(R.id._bg__group_2_ek1);
+		login_with_google = (TextView) findViewById(R.id.login_with_google);
+		pngwing_2 = (ImageView) findViewById(R.id.pngwing_2);
+//		_bg__group_3_ek1 = (View) findViewById(R.id._bg__group_3_ek1);
+//		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+//		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+//		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+//		ellipse_4 = (View) findViewById(R.id.ellipse_4);
+//		ellipse_5 = (View) findViewById(R.id.ellipse_5);
+//		ellipse_6 = (View) findViewById(R.id.ellipse_6);
+//		ellipse_7 = (View) findViewById(R.id.ellipse_7);
+//		ellipse_8 = (View) findViewById(R.id.ellipse_8);
+//		ellipse_9 = (View) findViewById(R.id.ellipse_9);
+//		ellipse_10 = (View) findViewById(R.id.ellipse_10);
+//		ellipse_11 = (View) findViewById(R.id.ellipse_11);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
